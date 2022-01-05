016ade0c2270c7fdc484447b79fa2a7fc2d2a12d
MahmoudKamal01 <53492856+MahmoudKamal01@users.noreply.github.com>
2022-01-05 16:34:57 +0200

    Adding condition function
    Condition function takes input from user and checks if it is 1 then, go to bubble sort; if it is 2 , then go to selection sort.

--- main.asm
+++ main.asm
@@ -43,3 +43,91 @@
      INT 21H                                  
  
   MAIN ENDP
+;-------------------CONDITION function-----------------------------------
+   CONDITION PROC 
+    push bx                       
+   
+    READ_SORT_NUMBER:
+ 
+     mov CX,0                   
+     mov BX,0                
+     mov AX,0                   
+     
+     Mov AH,1                     
+     INT 21h                     
+     
+     cmp    al, '-'             
+     JE NEGATIVE_INPUT                
+     
+     cmp al,'+'                   
+     jE POSITIVE_INPUT                     
+     jmp INPUT_ENTER                         
+              
+    NEGATIVE_INPUT:                    
+     INC CL                      
+     JMP BEEB_ERROR                  
+   
+    POSITIVE_INPUT:                  
+     INC CL                   
+     JMP BEEB_ERROR                
+                 
+    INPUT_ENTER:                    
+     MOV BL,AL                   
+     MOV AH, 1                 
+     INT 21H                    
+
+    SKIP_INPUT0:                
+     CMP AL, 0DH                
+     JE END_INPUT0                
+
+ 
+    MOVE_BACK0:                  
+     MOV AH, 2                    
+     MOV DL, 20H                
+     INT 21H                     
+     MOV DL, 8H                  
+     INT 21H                      
+     XOR DX, DX                  
+     DEC CL                     
+     JMP INPUT_ENTER                       
+
+     
+     PUSH AX                      
+     MOV AX, 10                   
+     MUL BX                       
+     MOV BX, AX                   
+     POP AX                   
+     ADD BX, AX                 
+     JS BEEB_ERROR                
+     JMP INPUT_ENTER                        
+                         
+    BEEB_ERROR:                      
+     MOV AH, 2                    
+     MOV DL, 7H               
+     INT 21H                     
+     
+    CLEAR_CHAR:                      
+     MOV DL, 8H                   
+     INT 21H                    
+     MOV DL, 20H                
+     INT 21H                   
+     MOV DL, 8H                   
+     INT 21H                 
+    LOOP CLEAR_CHAR                  
+    JMP READ_SORT_NUMBER                  
+       
+   END_INPUT0:               
+   
+   CHECK_FOR_BUBBLE:                 
+    CMP BL,31h                 
+    JE BUBBLE_SORT              
+    JNE CHECK_FOR_SELECTION          
+
+   CHECK_FOR_SELECTION:             
+    CMP BL,32h                 
+    JE SELECTION_SORT            
+    LEA DX, Msg5             
+    MOV AH, 9                     
+    INT 21H                    
+    MOV CX,1                   
+    Jne BEEB_ERROR                         
