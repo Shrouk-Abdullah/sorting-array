5fee6db1cc4fee821b50e647d035b4ceaa806495
sara823 <66432441+sara823@users.noreply.github.com>
2022-01-07 23:00:35 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -556,8 +556,7 @@ PRINT_ARRAY PROC
      MOV DL, 20H                 ;set DL=20H, ascii code of ' '                
      INT 21H                     ;interrupt 21H prints a character     
 
-     INC SI                      ; set SI=SI+1
-     INC SI                      ; set SI=SI+1
+      ADD SI, 2                   ; set SI=SI+2
    LOOP PRINTARRAY               ; if(CX!=0), jump to label PRINTARRAY
 
    POP DX                        ; pop a value from STACK into DX
